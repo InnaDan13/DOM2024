29028598768ebae7af6c7cdda713f0a40874d9ce
Inna Danylchuk <innadanylchuk13@gmail.com>
2024-06-24 20:38:06 +0200

    add fetch

--- assets/js/dataFetch.js
+++ assets/js/dataFetch.js
@@ -1,14 +1,54 @@
-"use strict"
+"use strict";
 
 const users = [
-    {id:1, name:'Test'},
-    {id:2,name:"John"}
-]
-
-const usersInJson = JSON.stringify(users)
+  { id: 1, name: "Test" },
+  { id: 2, name: "John" },
+];
+//js ->json
+const usersInJson = JSON.stringify(users);
 console.log(usersInJson);
+//-> [{"id":1,"name":"Test"},{"id":2,"name":"John"}]
 
-const usersFromJson = JSON.parse(usersInJson)
+//json - js
+
+const usersFromJson = JSON.parse(usersInJson);
 console.log(usersFromJson);
 
+const WEATHER_URL =
+  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,precipitation,wind_speed_10m&timezone=auto";
+//задає звідки прийдуть дані
 
+fetch(WEATHER_URL)
+  .then((response) => response.json()) // in then comes response
+  .then((data) => updateWeather(data))
+  .catch((error) => console.log(error));
+
+function updateWeather(weatherData) {
+  const {
+    current: { temperature_2m, wind_speed_10m },
+    current_units: {
+      temperature_2m: temperature_2m_units,
+      wind_speed_10m: wind_speed_10m_units
+    },
+  } = weatherData;
+  console.log(weatherData);
+  console.log(
+    `${weatherData.current.temperature_2m} ${weatherData.current_units.temperature_2m}`
+  );
+
+  const weatherArticle = document.querySelector(".weather");
+  weatherArticle.innerHTML = `
+  <p style="color:${calcTemperature(temperature_2m)}">
+  ${temperature_2m} ${temperature_2m_units}
+  </p>
+  <p>
+  ${wind_speed_10m} ${wind_speed_10m_units}
+  </p>`;
+}
+function calcTemperature(t) {
+  if (t > 25) {
+    return "red";
+  } else if (t < 25) {
+    return "green";
+  }
+}
